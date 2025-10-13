100e836e81c982ff621218e8b0e2deb16a11d4d7
piyush_kumar <piyushsinghlkr2002@gmail.com>
2025-10-13 20:45:30 +0530

    first commit

--- src/main/java/com/example/SpringActions/controller/SpringController.java
+++ src/main/java/com/example/SpringActions/controller/SpringController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/action")
 @CrossOrigin("*")
 public class SpringController {
 
